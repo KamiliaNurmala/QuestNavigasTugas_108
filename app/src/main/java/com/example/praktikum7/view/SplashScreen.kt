7ad1b7fa7ceeab7e639c4f0fb57329122c9bc4d6
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-07 03:29:03 +0700

    Menambahkan placeholder dan judul aplikasi

--- app/src/main/java/com/example/praktikum7/view/SplashScreen.kt
+++ app/src/main/java/com/example/praktikum7/view/SplashScreen.kt
@@ -13,5 +13,34 @@ fun SplashScreen(
             .background(Color(0xFFE1BEE7)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    )
+    ){
+        Text(
+            text = "Selamat Datang",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF6A1B9A)
+        )
+
+        Spacer(modifier = Modifier.height(40.dp))
+
+        Text(
+            text = "CARD-IST",
+            fontSize = 36.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF6A1B9A)
+        )
+
+        Text(
+            text = "MOBILE APP",
+            fontSize = 14.sp,
+            color = Color(0xFF6A1B9A)
+        )
+
+        Text(
+            text = "2025",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF6A1B9A)
+        )
+    }
 }
