426fbf58ed88083eda39c9567cb9febad213fcc3
KamiliaNurmala <nurmalakamilia@gmail.com>
2025-11-07 03:19:54 +0700

    Menambahkan layout dasar dan background

--- app/src/main/java/com/example/praktikum7/view/SplashScreen.kt
+++ app/src/main/java/com/example/praktikum7/view/SplashScreen.kt
@@ -3,6 +3,15 @@ package com.example.praktikum7.view
 import android.window.SplashScreen
 
 @Composable
-fun SplashScreen(){
-
+fun SplashScreen(
+    onNextButton: () -> Unit,
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(0xFFE1BEE7)),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
 }
